1ea0fabea838b6379af07ab228e50ef21934e395
lixin <2690688423@qq.com>
2021-01-02 21:07:34 +0800

    upgrade

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import NotFound from "../views/404.vue";
+import NProgress from "nprogress";
+import 'nprogress/nprogress.css';
 
 Vue.use(VueRouter);
 
@@ -29,25 +31,82 @@ const routes = [
     ]
   },
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path:"/",
+    component:() =>
+    import(/* webpackChunkName: "layout" */ "../layouts/BasicLayout"),
+    children:[
+      {
+        path:"/",
+        redirect:"../views/Dashboard/analysis.vue"
+      },
+      {
+        path: "/dashboard",
+        name: "dashboard",
+        component: { render: h => h("router-view") },
+        children: [
+          {
+            path: "/dashboard/analysis",
+            name: "analysis",
+            component: () =>
+              import(/* webpackChunkName: "dashboard" */ "../views/Dashboard/Analysis")
+          }
+        ]
+      },
+      //form
+      {
+        path: "/form/step-form",
+        name: "stepform",
+        component: () =>
+          import(/* webpackChunkName: "form" */ "../views/Forms/StepForm"),
+        children: [
+          {
+            path: "/form/step-form",
+            redirect: "/form/step-form/info"
+          },
+          {
+            path: "/form/step-form/info",
+            name: "info",
+            component: () =>
+              import(/* webpackChunkName: "form" */ "../views/Forms/StepForm/Step1")
+          },
+          {
+            path: "/form/step-form/confirm",
+            name: "confirm",
+            component: () =>
+              import(/* webpackChunkName: "form" */ "../views/Forms/StepForm/Step2")
+          },
+          {
+            path: "/form/step-form/result",
+            name: "result",
+            component: () =>
+              import(/* webpackChunkName: "form" */ "../views/Forms/StepForm/Step3")
+          }
+        ]
+      },
+      {
+      path: "*",
+      name: "404",
+      component: NotFound
+      }
+    ]
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
 ];
+
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
 });
+router.beforeEach((to, form, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
+});
+
+
 
 export default router;
